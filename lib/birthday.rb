54a7ea40eb83027ce10cb1424da4f33224983fcf
Mason Phillips <mjphillips2000@gmail.com>
2020-10-19 19:12:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,15 +5,10 @@
 # }
 
 def happy_birthday(birthday_kids)
-<<<<<<< HEAD
-=======
-def happy_birthday(birthday_kids)
->>>>>>> b9a57ad6f33d0f8e34c1a67d9c320cc7064778a3
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
-<<<<<<< HEAD
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
@@ -23,8 +18,6 @@ def age_appropriate_birthday(birthday_kids)
       puts "You are too old for this."
     end
   end
-=======
->>>>>>> b9a57ad6f33d0f8e34c1a67d9c320cc7064778a3
 end
 
 
